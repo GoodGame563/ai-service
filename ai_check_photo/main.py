763d28170b9bf4b3d56118c616fe6d4b7647ae24
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-11-19 23:05:54 +0300

    main

---
+++ ai_check_photo/main.py
@@ -0,0 +1,94 @@
+from PIL import Image, UnidentifiedImageError
+from io import BytesIO
+from pydantic import BaseModel
+from transformers import AutoImageProcessor, AutoModelForImageClassification
+
+import pika
+import json
+import requests
+import torch
+from transformers import AutoImageProcessor, AutoModelForImageClassification
+
+
+class Element(BaseModel):
+    type: str
+    url: str
+
+
+
+print(torch.cuda.is_available())
+print(f"Is CUDA supported by this system? {torch.cuda.is_available()}")
+print(f"CUDA version: {torch.version.cuda}")
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+model_name = "miguelcarv/resnet-152-text-detector"
+processor_name = "microsoft/resnet-50"
+
+model_directory = f"{model_name.split('/')[1]}-ai"
+
+try:
+    model = AutoModelForImageClassification.from_pretrained(model_directory,).to(device)
+    processor = AutoImageProcessor.from_pretrained(model_directory, do_resize=False)
+except:
+    model = AutoModelForImageClassification.from_pretrained(model_name,).to(device)
+    model.save_pretrained(model_directory)
+    processor = AutoImageProcessor.from_pretrained(processor_name, do_resize=False)
+    processor.save_pretrained(model_directory)
+    
+
+def callback(ch, method, properties, body):
+    message = Element(**json.loads(body))
+    try:
+        response = requests.get(message.url, timeout=3)
+        response.raise_for_status()
+        image = Image.open(BytesIO(response.content)).convert('RGB').resize((300,300))
+        inputs = processor(image, return_tensors="pt").to(device).pixel_values
+        with torch.no_grad():
+            outputs = model(inputs)
+            
+        logits_per_image = outputs.logits 
+        logits_per_image = logits_per_image.cpu()
+        probs = logits_per_image.softmax(dim=1) 
+        print(bool(probs[0][0] < probs[0][1]))
+
+    except UnidentifiedImageError:
+        print(f"Error: The URL did not return a valid image format. URL: {message.url}")
+        return
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err} - URL: {message.url}")
+        return
+    except requests.exceptions.ConnectionError as conn_err:
+        print(f"Connection error occurred: {conn_err} - URL: {message.url}")
+        return
+    except requests.exceptions.Timeout as timeout_err:
+        print(f"Timeout error occurred: {timeout_err} - URL: {message.url}")
+        return
+    except Exception as ex:
+        print(f"Error processing message: {ex}")
+        return
+     
+    
+
+def start_definition_text_consumer():
+    credentials = pika.PlainCredentials('guest', 'guest')
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq', 5672))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='task_exchange', exchange_type='direct')
+
+    queue_name = 'ai_check_photo_queue'
+    channel.queue_declare(queue=queue_name)
+
+    channel.queue_bind(exchange='task_exchange', queue=queue_name, routing_key='definition_text')
+
+    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+
+    print("Definition text consumer waiting for messages...")
+    channel.start_consuming()
+
+if __name__ == "__main__":
+    start_definition_text_consumer()
