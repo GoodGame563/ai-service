036e8b4bef30c16813eadf94c49fb321aabd30b0
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-11-27 23:34:46 +0300

    asdasd

--- ai_multy_modal/main.py
+++ ai_multy_modal/main.py
@@ -1,170 +1,121 @@
-# from PIL import Image
-# import os
-
-# import torch
-# from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
-# from qwen_vl_utils import process_vision_info
-
-# from threading import Thread
-# import requests
-# import asyncio
-# import aiohttp
-
-# import json
-# from pydantic import BaseModel
-
-# from tqdm import tqdm
-# import datetime
-
-# class Element(BaseModel):
-#     Images: str
-#     Objects_in_Image_by_Words: list
-#     Objects_in_Image_by_Words_EN: list
-#     Description_of_Image_in_Sentence: str
-#     Questions_for_Image: list
-#     Original_Description:str
-#     The_text_in_the_picture:list
-#     Words_in_the_picture:list
-#     Text_exsist:bool
-#     Human_exsist:bool
-
-
-# print(torch.cuda.is_available())
-# print(f"Is CUDA supported by this system? {torch.cuda.is_available()}")
-# print(f"CUDA version: {torch.version.cuda}")
-
-# if torch.cuda.is_available():
-#     device = torch.device("cuda")
-# else:
-#     device = torch.device("cpu")
-
-# path_to_json = "../output.json"
-# model_name = "Qwen/Qwen2-VL-2B-Instruct"
-
-# model_directory = f"{model_name.split('/')[1]}"
-
-
-# try:
-#     model = Qwen2VLForConditionalGeneration.from_pretrained(model_directory, torch_dtype="auto", device_map="auto")
-#     processor = AutoProcessor.from_pretrained(model_directory)
-# except:
-#     model = Qwen2VLForConditionalGeneration.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
-#     processor = AutoProcessor.from_pretrained(model_name)
-#     processor.save_pretrained(model_directory)
-#     model.save_pretrained(model_directory)
-
-
-# execution_times_sec = []
-# # model = model.to(device)
-# # model = CLIPModel.from_pretrained().to(device)
-# # processor = CLIPProcessor.from_pretrained()
-   
-# # outputs = model(**inputs)
-# # logits_per_image = outputs.logits_per_image 
-# # logits_per_image = logits_per_image.cpu()
-    
-# messages = [
-#     {
-#         "role": "user",
-#         "content": [
-#             {
-#                 "type": "image",
-#                 "image": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg",
-#             },
-#             {"type": "text", "text": "Describe this image."},
-#         ],
-#     }
-# ]
-
-# text = processor.apply_chat_template(
-#     messages, tokenize=False, add_generation_prompt=True
-# )
-# image_inputs, video_inputs = process_vision_info(messages)
-# inputs = processor(
-#     text=[text],
-#     images=image_inputs,
-#     videos=video_inputs,
-#     padding=True,
-#     return_tensors="pt",
-# )
-# inputs = inputs.to(device)
-
-# generated_ids = model.generate(**inputs, max_new_tokens=128)
-# generated_ids_trimmed = [
-#     out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-# ]
-# output_text = processor.batch_decode(
-#     generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
-# )
-# print(output_text)
-# print("end")
-
-# # with open(path_to_json, "r", encoding="utf-8") as f:
-# #     data = json.load(f)
-# #     for element in tqdm(data):
-# #         element = Element(**element)
-# #         start = datetime.datetime.now()
-
-
-
-        
-# #         end = datetime.datetime.now()
-# #         execution_time = (end - start).total_seconds()
-# #         execution_times_sec.append(execution_time)
-
-
-
-# #     average_time_sec = sum(execution_times_sec) / len(data)
-# #     average_time_ms = average_time_sec * 1000
-# #     print(f"Среднее время выполнения datetime.now() в секундах: {average_time_sec:.10f} сек")
-# #     print(f"Среднее время выполнения datetime.now() в миллисекундах: {average_time_ms:.10f} мс")
-
 from PIL import Image
-import requests
+import os
+
 import torch
-from torchvision import io
-from typing import Dict
 from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
+from qwen_vl_utils import process_vision_info
 
-# Load the model in half-precision on the available device(s)
-model = Qwen2VLForConditionalGeneration.from_pretrained(
-    "Qwen/Qwen2-VL-2B-Instruct", torch_dtype="auto", device_map="auto"
-)
-processor = AutoProcessor.from_pretrained("Qwen/Qwen2-VL-2B-Instruct")
+from threading import Thread
+import requests
+import asyncio
+import aiohttp
 
-# Image
-url = "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg"
-image = Image.open(requests.get(url, stream=True).raw)
+import json
+from pydantic import BaseModel
 
-conversation = [
+from tqdm import tqdm
+import datetime
+
+class Element(BaseModel):
+    Images: str
+    Objects_in_Image_by_Words: list
+    Objects_in_Image_by_Words_EN: list
+    Description_of_Image_in_Sentence: str
+    Questions_for_Image: list
+    Original_Description:str
+    The_text_in_the_picture:list
+    Words_in_the_picture:list
+    Text_exsist:bool
+    Human_exsist:bool
+
+
+print(torch.cuda.is_available())
+print(f"Is CUDA supported by this system? {torch.cuda.is_available()}")
+print(f"CUDA version: {torch.version.cuda}")
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+path_to_json = "../output.json"
+model_name = "Qwen/Qwen2-VL-2B-Instruct"
+
+model_directory = f"{model_name.split('/')[1]}"
+
+
+try:
+    model = Qwen2VLForConditionalGeneration.from_pretrained(model_directory, torch_dtype="auto", device_map="auto")
+    processor = AutoProcessor.from_pretrained(model_directory)
+except:
+    model = Qwen2VLForConditionalGeneration.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
+    processor = AutoProcessor.from_pretrained(model_name)
+    processor.save_pretrained(model_directory)
+    model.save_pretrained(model_directory)
+
+
+execution_times_sec = []
+# model = model.to(device)
+# model = CLIPModel.from_pretrained().to(device)
+# processor = CLIPProcessor.from_pretrained()
+   
+# outputs = model(**inputs)
+# logits_per_image = outputs.logits_per_image 
+# logits_per_image = logits_per_image.cpu()
+    
+messages = [
     {
         "role": "user",
         "content": [
             {
                 "type": "image",
+                "image": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg",
             },
             {"type": "text", "text": "Describe this image."},
         ],
     }
 ]
 
-
-# Preprocess the inputs
-text_prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
-# Excepted output: '<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n<|im_start|>user\n<|vision_start|><|image_pad|><|vision_end|>Describe this image.<|im_end|>\n<|im_start|>assistant\n'
-
-inputs = processor(
-    text=[text_prompt], images=[image], padding=True, return_tensors="pt"
+text = processor.apply_chat_template(
+    messages, tokenize=False, add_generation_prompt=True
 )
-inputs = inputs.to("cuda")
+image_inputs, video_inputs = process_vision_info(messages)
+inputs = processor(
+    text=[text],
+    images=image_inputs,
+    videos=video_inputs,
+    padding=True,
+    return_tensors="pt",
+)
+inputs = inputs.to(device)
 
-# Inference: Generation of the output
-output_ids = model.generate(**inputs, max_new_tokens=128)
-generated_ids = [
-    output_ids[len(input_ids) :]
-    for input_ids, output_ids in zip(inputs.input_ids, output_ids)
+generated_ids = model.generate(**inputs, max_new_tokens=128)
+generated_ids_trimmed = [
+    out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
 ]
 output_text = processor.batch_decode(
-    generated_ids, skip_special_tokens=True, clean_up_tokenization_spaces=True
+    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
 )
 print(output_text)
+print("end")
+
+# with open(path_to_json, "r", encoding="utf-8") as f:
+#     data = json.load(f)
+#     for element in tqdm(data):
+#         element = Element(**element)
+#         start = datetime.datetime.now()
+
+
+
+        
+#         end = datetime.datetime.now()
+#         execution_time = (end - start).total_seconds()
+#         execution_times_sec.append(execution_time)
+
+
+
+#     average_time_sec = sum(execution_times_sec) / len(data)
+#     average_time_ms = average_time_sec * 1000
+#     print(f"Среднее время выполнения datetime.now() в секундах: {average_time_sec:.10f} сек")
+#     print(f"Среднее время выполнения datetime.now() в миллисекундах: {average_time_ms:.10f} мс")
+
