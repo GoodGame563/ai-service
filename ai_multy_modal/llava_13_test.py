c463134b4b108426c9dd442887d7b7a6a17503f6
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-11-28 19:05:56 +0300

    Create llava_13_test.py

---
+++ ai_multy_modal/llava_13_test.py
@@ -0,0 +1,45 @@
+from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
+import torch
+from PIL import Image
+import requests
+
+# processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-34b-hf")
+
+# model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llava-v1.6-34b-hf", torch_dtype=torch.float16, low_cpu_mem_usage=True) 
+# model.to("cuda:0")
+model_name = "llava-hf/llava-v1.6-vicuna-13b-hf"
+
+model_directory = f"{model_name.split('/')[1]}"
+try:
+    model = LlavaNextForConditionalGeneration.from_pretrained(model_directory, torch_dtype="auto", device_map="auto").to("cuda:0")
+    processor = LlavaNextProcessor.from_pretrained(model_directory)
+except:
+    model = LlavaNextForConditionalGeneration.from_pretrained(model_name, torch_dtype="auto", device_map="auto").to("cuda:0")
+    processor = LlavaNextProcessor.from_pretrained(model_name)
+    processor.save_pretrained(model_directory)
+    model.save_pretrained(model_directory)
+
+# prepare image and text prompt, using the appropriate prompt template
+url = "https://github.com/haotian-liu/LLaVA/blob/1a91fc274d7c35a9b50b3cb29c4247ae5837ce39/images/llava_v1_5_radar.jpg?raw=true"
+image = Image.open(requests.get(url, stream=True).raw)
+
+# Define a chat histiry and use `apply_chat_template` to get correctly formatted prompt
+# Each value in "content" has to be a list of dicts with types ("text", "image") 
+conversation = [
+    {
+
+      "role": "user",
+      "content": [
+          {"type": "text", "text": "Опиши все, что изображено на предоставленной картинке. Укажи объекты, их взаимное расположение, цвета, размеры, действия (если изображено движение), а также стиль изображения. Ответ должен быть исключительно на русском языке, максимально детализированным и развернутым."},
+          {"type": "image"},
+        ],
+    },
+]
+prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
+
+inputs = processor(images=image, text=prompt, return_tensors="pt").to("cuda:0")
+
+# autoregressively complete prompt
+output = model.generate(**inputs, max_new_tokens=100)
+
+print(processor.decode(output[0], skip_special_tokens=True))
