58c0a6b6ff4f03e99c80d7e0ed3ab62f92ce277b
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-11-28 20:35:34 +0300

    Update llava_13_test.py

--- ai_multy_modal/llava_13_test.py
+++ ai_multy_modal/llava_13_test.py
@@ -2,11 +2,12 @@ from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
 import torch
 from PIL import Image
 import requests
+import sentencepiece
 
 
+processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-vicuna-13b-hf", trust_remote_code=True, use_fast=False)
 
 model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llava-v1.6-vicuna-13b-hf", torch_dtype=torch.float16, low_cpu_mem_usage=True)
-processor = LlavaNextProcessor.from_pretrained("llava-hf/llava-v1.6-vicuna-13b-hf", trust_remote_code=True, use_fast=False)
 model.to("cuda:0")
 
 # prepare image and text prompt, using the appropriate prompt template
