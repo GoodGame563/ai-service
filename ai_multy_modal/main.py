9c8c2e0b31217ed7d4d6ca5feada07d3c8c1f093
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-12-01 20:48:40 +0300

    Update main.py

--- ai_multy_modal/main.py
+++ ai_multy_modal/main.py
@@ -12,23 +12,24 @@ image = Image.open(url)
 
 # Define a chat histiry and use `apply_chat_template` to get correctly formatted prompt
 # Each value in "content" has to be a list of dicts with types ("text", "image") 
-conversation = [
-    {
+# conversation = [
+#     {
 
-      "role": "system",
-      "content": [
-          {"type": "text", "text": "Тебе нужно отвечать только на русском языке если не просят обратное."},
-        ],
-    },
-    {
+#       "role": "system",
+#       "content": [
+#           {"type": "text", "text": "Тебе нужно отвечать только на русском языке если не просят обратное."},
+#         ],
+#     },
+#     {
 
-      "role": "user",
-      "content": [
-          {"type": "text", "text": "Расскажи о себе"},
-        ],
-    },
-]
-prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
+#       "role": "user",
+#       "content": [
+#           {"type": "text", "text": "Расскажи о себе"},
+#         ],
+#     },
+# ]
+# prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
+prompt = "Explain the importance of renewable energy in modern society."
 
 # inputs = processor(images=image, text=prompt, return_tensors="pt").to(model.device)
 inputs = processor(text=prompt, return_tensors="pt").to(model.device)
