540205fc843ac73f2e480b6fecbfd48fd7b3aabe
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-12-01 21:00:04 +0300

    dsfsd

--- ai_multy_modal/main.py
+++ ai_multy_modal/main.py
@@ -12,27 +12,18 @@ image = Image.open(url)
 
 # Define a chat histiry and use `apply_chat_template` to get correctly formatted prompt
 # Each value in "content" has to be a list of dicts with types ("text", "image") 
-# conversation = [
-#     {
+conversation = [
+    {
 
-#       "role": "system",
-#       "content": [
-#           {"type": "text", "text": "Тебе нужно отвечать только на русском языке если не просят обратное."},
-#         ],
-#     },
-#     {
+      "role": "user",
+      "content": [
+          {"type": "text", "text": "Расскажи не сливается ли текст с фоном на данной фотографии? Если текст сливается напиши яблоко, а если нет то напиши банан"},
+        ],
+    },
+]
+prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
 
-#       "role": "user",
-#       "content": [
-#           {"type": "text", "text": "Расскажи о себе"},
-#         ],
-#     },
-# ]
-# prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
-prompt = "Explain the importance of renewable energy in modern society."
-
-# inputs = processor(images=image, text=prompt, return_tensors="pt").to(model.device)
-inputs = processor(text=prompt,images=None, return_tensors="pt").to(model.device)
+inputs = processor(images=image, text=prompt, return_tensors="pt").to(model.device)
 
 # autoregressively complete prompt
 output = model.generate(**inputs, max_new_tokens=800)
