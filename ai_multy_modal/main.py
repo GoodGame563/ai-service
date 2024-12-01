fca4b2c1161c1d9d9c72381c3122c31525343cbe
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-12-01 20:35:40 +0300

    фывфы

--- ai_multy_modal/main.py
+++ ai_multy_modal/main.py
@@ -1,123 +1,38 @@
-from PIL import Image
-import os
-
+from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration
 import torch
-from transformers import Qwen2VLForConditionalGeneration, AutoTokenizer, AutoProcessor
-from qwen_vl_utils import process_vision_info
-
-from threading import Thread
+from PIL import Image
 import requests
-import asyncio
-import aiohttp
 
-import json
-from pydantic import BaseModel
+processor = LlavaNextProcessor.from_pretrained("llava-hf/llama3-llava-next-8b-hf")
+model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llama3-llava-next-8b-hf", torch_dtype=torch.float16, device_map="auto") 
 
-from tqdm import tqdm
-import datetime
-os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+# prepare image and text prompt, using the appropriate prompt template
+url = "image/2.webp"
+image = Image.open(url)
 
-class Element(BaseModel):
-    Images: str
-    Objects_in_Image_by_Words: list
-    Objects_in_Image_by_Words_EN: list
-    Description_of_Image_in_Sentence: str
-    Questions_for_Image: list
-    Original_Description:str
-    The_text_in_the_picture:list
-    Words_in_the_picture:list
-    Text_exsist:bool
-    Human_exsist:bool
-
-torch.cuda.empty_cache()  # Очистка кэша
-torch.cuda.reset_peak_memory_stats()  # Сброс статистики
-print(torch.cuda.is_available())
-print(f"Is CUDA supported by this system? {torch.cuda.is_available()}")
-print(f"CUDA version: {torch.version.cuda}")
-
-if torch.cuda.is_available():
-    device = torch.device("cuda")
-else:
-    device = torch.device("cpu")
-
-path_to_json = "../output.json"
-model_name = "Qwen/Qwen2-VL-2B-Instruct"
-
-model_directory = f"{model_name.split('/')[1]}"
-
-
-try:
-    model = Qwen2VLForConditionalGeneration.from_pretrained(model_directory, torch_dtype="auto", device_map="auto")
-    processor = AutoProcessor.from_pretrained(model_directory)
-except:
-    model = Qwen2VLForConditionalGeneration.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
-    processor = AutoProcessor.from_pretrained(model_name)
-    processor.save_pretrained(model_directory)
-    model.save_pretrained(model_directory)
-
-
-execution_times_sec = []
-# model = model.to(device)
-# model = CLIPModel.from_pretrained().to(device)
-# processor = CLIPProcessor.from_pretrained()
-   
-# outputs = model(**inputs)
-# logits_per_image = outputs.logits_per_image 
-# logits_per_image = logits_per_image.cpu()
-    
-messages = [
+# Define a chat histiry and use `apply_chat_template` to get correctly formatted prompt
+# Each value in "content" has to be a list of dicts with types ("text", "image") 
+conversation = [
     {
-        "role": "user",
-        "content": [
-            {
-                "type": "image",
-                "image": "https://qianwen-res.oss-cn-beijing.aliyuncs.com/Qwen-VL/assets/demo.jpeg",
-            },
-            {"type": "text", "text": "Describe this image."},
+
+      "role": "system",
+      "content": [
+          {"type": "text", "text": "Тебе нужно отвечать только на русском языке если не просят обратное."},
         ],
-    }
+    },
+    {
+
+      "role": "user",
+      "content": [
+          {"type": "text", "text": "Расскажи о себе"},
+        ],
+    },
 ]
+prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
 
-text = processor.apply_chat_template(
-    messages, tokenize=False, add_generation_prompt=True
-)
-image_inputs, video_inputs = process_vision_info(messages)
-inputs = processor(
-    text=[text],
-    images=image_inputs,
-    videos=video_inputs,
-    padding=True,
-    return_tensors="pt",
-)
-inputs = inputs.to(device)
+inputs = processor(images=image, text=prompt, return_tensors="pt").to(model.device)
 
-generated_ids = model.generate(**inputs, max_new_tokens=128)
-generated_ids_trimmed = [
-    out_ids[len(in_ids) :] for in_ids, out_ids in zip(inputs.input_ids, generated_ids)
-]
-output_text = processor.batch_decode(
-    generated_ids_trimmed, skip_special_tokens=True, clean_up_tokenization_spaces=False
-)
-print(output_text)
-print("end")
+# autoregressively complete prompt
+output = model.generate(**inputs, max_new_tokens=800)
 
-# with open(path_to_json, "r", encoding="utf-8") as f:
-#     data = json.load(f)
-#     for element in tqdm(data):
-#         element = Element(**element)
-#         start = datetime.datetime.now()
-
-
-
-        
-#         end = datetime.datetime.now()
-#         execution_time = (end - start).total_seconds()
-#         execution_times_sec.append(execution_time)
-
-
-
-#     average_time_sec = sum(execution_times_sec) / len(data)
-#     average_time_ms = average_time_sec * 1000
-#     print(f"Среднее время выполнения datetime.now() в секундах: {average_time_sec:.10f} сек")
-#     print(f"Среднее время выполнения datetime.now() в миллисекундах: {average_time_ms:.10f} мс")
-
+print(processor.decode(output[0], skip_special_tokens=True))
