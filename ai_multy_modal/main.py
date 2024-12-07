711b66004bfa59f444faef2a6c4d42c435346ad9
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-12-07 18:00:58 +0300

    dsfds

--- ai_multy_modal/main.py
+++ ai_multy_modal/main.py
@@ -13,7 +13,6 @@ image = Image.open(url)
 # Define a chat histiry and use `apply_chat_template` to get correctly formatted prompt
 # Each value in "content" has to be a list of dicts with types ("text", "image") 
 conversation = [
-    {"role": "system", "content": "Ты модель которая отвечает True or False."},
     {
       "role": "user",
       "content": [
