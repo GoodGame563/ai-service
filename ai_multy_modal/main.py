2879888c9402ef793738a4b342035e58d73862ec
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-12-13 15:07:02 +0300

    zxcxz

--- ai_multy_modal/main.py
+++ ai_multy_modal/main.py
@@ -4,19 +4,18 @@ from PIL import Image
 import requests
 
 processor = LlavaNextProcessor.from_pretrained("llava-hf/llama3-llava-next-8b-hf")
-model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llama3-llava-next-8b-hf", torch_dtype=torch.float16, device_map="auto") 
+model = LlavaNextForConditionalGeneration.from_pretrained("llava-hf/llama3-llava-next-8b-hf", torch_dtype=torch.float16, device_map="auto")
 
 # prepare image and text prompt, using the appropriate prompt template
-url = "image/2.webp"
+url = "image/5.webp"
 image = Image.open(url)
 
-# Define a chat histiry and use `apply_chat_template` to get correctly formatted prompt
-# Each value in "content" has to be a list of dicts with types ("text", "image") 
+
 conversation = [
     {
       "role": "user",
       "content": [
-          {"type": "text", "text": "Расскажи не сливается ли текст с фоном на данной фотографии?"},
+          {"type": "text", "text": "Проанализируй текст на карточке товара и предложи конкретные советы по улучшению его читаемости. Например, предложи увеличить размер шрифта, изменить цвет шрифта или настроить интервал между строками. Убедись, что рекомендации включают точные значения (например, 'Увеличить размер шрифта до 16px' или 'Изменить цвет на #FFFFFF')."},
           {"type": "image"},
         ],
     },
@@ -25,7 +24,6 @@ prompt = processor.apply_chat_template(conversation, add_generation_prompt=True)
 
 inputs = processor(images=image, text=prompt, return_tensors="pt").to(model.device)
 
-# autoregressively complete prompt
-output = model.generate(**inputs, max_new_tokens=800)
+output = model.generate(**inputs, max_new_tokens=200)
 
 print(processor.decode(output[0], skip_special_tokens=True))
--- ai_text_operation/main.py
+++ ai_text_operation/main.py
@@ -1,28 +1,60 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torchvision.models as models
-import torch
 from pydantic import BaseModel
+from dotenv import load_dotenv
+
+import sys
+import os
+
+sys.path.append(os.path.join(os.getcwd(), '..'))
+os.system('python -m grpc_tools.protoc -I. --python_out=. --grpc_python_out=. task.proto')
+
+
+import grpc
+import task_pb2
+import task_pb2_grpc
+import pika
+import torch
 import json
 
-class description_words(BaseModel):
-    description: str
-    words: list[str]
 
-# Путь к локальной модели
-model_directory = "./qwen_model"
+load_dotenv()
+
+class SeoWord(BaseModel):
+    keyword_id: str
+    raw_keyword: str
+    freq: int
+    position: int
+
+class Product(BaseModel):
+    id: int 
+    name: str
+    description: str
+    seo: list[SeoWord] 
+    
+class SeoMessage(BaseModel):
+    id: int
+    type: str
+    product: Product
+    competitors: list[Product]
+
+class ReviewsMessage(BaseModel):
+    id: int
+    type: str
+    reviews: list[str]
+
+
+model_directory = "./qwen_model-ai"
 try:
-# Загрузка модели и токенизатора
     tokenizer = AutoTokenizer.from_pretrained(model_directory)
     model = AutoModelForCausalLM.from_pretrained(model_directory)
 except:
     model_name = "Qwen/Qwen2.5-1.5B-Instruct"
     local_directory = "./qwen_model"
 
-    # Загрузка модели и токенизатора с Hugging Face
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_pretrained(model_name)
 
-    # Сохранение модели и токенизатора локально
     tokenizer.save_pretrained(local_directory)
     model.save_pretrained(local_directory)
 
@@ -93,10 +125,10 @@ def generate_new_text_without_seo_words(text: str)->str:
     final = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
     return final
 
-def generate_new_text_with_seo_words_v1(main_element:description_words, elements:list[description_words]) -> str:
-    prompt = f"Прочитай описание нашего товара и список ключевых слов:\nНаше описание: {main_element.description}\nНаши ключевые слова: {main_element.words}\nПрочитай описание и ключевые слова конкурентов (без упоминания их брендов):"
+def generate_new_text_with_seo_words_v1(main_element:Product, elements:list[Product]) -> str:
+    prompt = f"Прочитай описание нашего товара и список ключевых слов:\nНаше описание: {main_element.description}\nНаши ключевые слова: {[word.raw_keyword for word in main_element.seo]}\nПрочитай описание и ключевые слова конкурентов (без упоминания их брендов):"
     for element in  elements:
-        prompt += f"\nОписание конкурента: {element.description}\nКлючевые слова конкурента: {element.words}"
+        prompt += f"\nОписание конкурента: {element.description}\nКлючевые слова конкурента: {[word.raw_keyword for word in element.seo]}"
     
     messages = [
         {"role": "system", "content": """
@@ -135,10 +167,10 @@ def generate_new_text_with_seo_words_v1(main_element:description_words, elements
     return final
 
 
-def generate_new_text_with_seo_words_v2(main_element:description_words, elements:list[description_words]) -> str:
-    prompt = f"Прочитай описание нашего товара и список ключевых слов:\nНаше описание: {main_element.description}\nНаши ключевые слова: {main_element.words}\nПрочитай описание и ключевые слова конкурентов (без упоминания их брендов):"
+def generate_new_text_with_seo_words_v2(main_element:Product, elements:list[Product]) -> str:
+    prompt = f"Прочитай описание нашего товара и список ключевых слов:\nНаше описание: {main_element.description}\nНаши ключевые слова: {[word.raw_keyword for word in main_element.seo]}\nПрочитай описание и ключевые слова конкурентов (без упоминания их брендов):"
     for element in  elements:
-        prompt += f"\nОписание конкурента: {element.description}\nКлючевые слова конкурента: {element.words}"
+        prompt += f"\nОписание конкурента: {element.description}\nКлючевые слова конкурента: {[word.raw_keyword for word in element.seo]}"
     
     messages = [
         {"role": "system", "content": """
@@ -180,6 +212,41 @@ def generate_new_text_with_seo_words_v2(main_element:description_words, elements
     final = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
     return final
 
+
+
+def generate_by_reviews(reviews:list[str]) -> str:
+    reviews_str = ""
+    for review in reviews:
+        reviews_str += f"{review}, "
+    prompt = f"Отзывы {reviews_str}"
+   
+    
+    messages = [
+        {"role": "system", "content": """Ты проффесиональный анализатор отзывов о товаре на маркетплейсе
+         Проанализируй отзывы покупателей на товары конкурентов. Определи 10 наиболее часто упоминаемых преимуществ и основываясь на отзывах товара. Опиши 10 ключевых характеристик и 10 преимуществ, которые делают их популярными среди пользователей. 
+         Выпиши, что больше всего не нравится клиентам в использовании товара. 
+         Напиши список того, что больше всего не нравится теми словами, как пишут в отзывах, расположив популярность от популярных к менее популярным. 
+         После завершения анализа, выдели сегменты целевой аудитории на основе ответов из отзывов"""},
+        {"role": "user", "content": prompt}
+    ]
+    text = tokenizer.apply_chat_template(
+        messages,
+        tokenize=False,
+        add_generation_prompt=True
+    )
+    model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+    generated_ids = model.generate(
+        **model_inputs,
+        temperature=0.8,
+        max_new_tokens=4096
+    )
+    generated_ids = [
+        output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+    ]
+    final = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+    return final
+
 # text = "Профессиональный спрей кондиционер для волос ULAB станет незаменимым помощником в ежедневном уходе за волосами. Этот продукт обеспечивает надежную термозащиту, разглаживание, увлажнение и восстановление. \n\nСпрей для волос обеспечивает защиту при укладке, благодаря чему волосы остаются гладкими и мягкими. Термозащитный эффект спрея предотвращает повреждение волос, сохраняя их силу и блеск. Этот спрей также защищает волосы от УФ-лучей.\n\nСредство солнцезащитное способствует легкому расчесыванию и укладке, благодаря чему волосы становятся более послушными. Спрей придает объем у корней для прикорневого объема, делая волосы густыми. Если ваши волосы склонны к пушистости или секущимся кончикам, этот спрей поможет разгладить их и придать волосам ухоженный вид. Эффект ламинирования, который дает спрей, делает волосы шелковистыми и блестящими.\n\nПарфюмированный спрей идеально подходит для использования на поврежденных, тонких и вьющихся волосах, обеспечивая им дополнительное питание и увлажнение, и защиту от солнца. Кератин в составе спрея способствует восстановлению структуры волос.\n\nЭтот кератиновый разглаживающий спрей для волос подходит для ухода за кожей головы, предотвращая сухость и шелушение. Он обеспечивает комплексный уход, питая волосы от корней до кончиков. \n\nЖенский спрей станет отличным выбором для тех, кто стремится к салонному уходу, к гладкости и блеску волос. Станет прекрасным подарком для женщин и девушек.\n\nСухие, поврежденные, пушистые или тонкие - многофункциональный спрей юлаб 10 в 1 обеспечит уход и восстановление. \n\nПрофессиональный несмываемый термоспрей для волос, особенно для окрашенных, обеспечивает защиту и эластичность, предотвращая электризацию и антистатик. Он идеален после утюжка и фена, сохраняя эффект кератинового выпрямления. Этот бьюти продукт незаменим для здорового и красивого вида волос."
 # generate_new_text_without_seo_words(text)
 # print(len(text.split()))
@@ -190,18 +257,115 @@ def generate_new_text_with_seo_words_v2(main_element:description_words, elements
 
 # print(f"True: {t}, False: {f}")
 
-main_element = description_words(description="",words=[])
-elements = []
-
-with open("response.json", "r", encoding='utf-8') as write_file:
-    t = list(json.load(write_file))
-    words = []
+# main_element = description_words(description="",words=[])
+# elements = []
+# with open("response.json", "r", encoding='utf-8') as write_file:
+#     t = list(json.load(write_file))
+#     words = []
     
-    first_element = t.pop()
-    main_element.description = first_element["description"]
-    main_element.words = [e["raw_keyword"] for e in first_element["seo"]]
-    for element in t:
-        elements.append(description_words(description=element["description"], words=[e["raw_keyword"] for e in element["seo"]]))
+#     first_element = t.pop()
+#     main_element.description = first_element["description"]
+#     main_element.words = [e["raw_keyword"] for e in first_element["seo"]]
+#     for element in t:
+#         elements.append(description_words(description=element["description"], words=[e["raw_keyword"] for e in element["seo"]]))
+# print(generate_new_text_with_seo_words_v2(main_element, elements))
 
-print(generate_new_text_with_seo_words_v2(main_element, elements))
+# with open("response.json", "r", encoding='utf-8') as write_file:
+#     t = list(json.load(write_file))
+#     first_element = t.pop()
+#     reviews = first_element['reviews']
+#     print(generate_by_reviews(reviews))
 
+def send_answer_to_description(success: bool, message:str, data: task_pb2.CheckDescriptionData):
+    try:
+        with grpc.insecure_channel(f"{os.getenv('GRPC_HOST')}:{os.getenv('GRPC_PORT')}") as channel:
+            stub = task_pb2_grpc.TaskServiceStub(channel)
+            request = task_pb2.UpdateDescriptionRequest(
+                success=success,
+                message=message,
+                data=data
+            )
+            print(stub.UpdateDescription(request))
+    except grpc.RpcError as e:
+        print(f"Error connecting to gRPC server: {e}")
+        return
+    
+def send_answer_to_reviews(success: bool, message:str, data: task_pb2.CheckReviewsData):
+    try:
+        with grpc.insecure_channel(f"{os.getenv('GRPC_HOST')}:{os.getenv('GRPC_PORT')}") as channel:
+            stub = task_pb2_grpc.TaskServiceStub(channel)
+            request = task_pb2.UpdateReviewsAnalyzeRequest(
+                success=success,
+                message=message,
+                data=data
+            )
+            print(stub.UpdateReviewsAnalyze(request))
+    except grpc.RpcError as e:
+        print(f"Error connecting to gRPC server: {e}")
+        return
+
+
+
+def callback(ch, method, properties, body):
+    raw_type_message = json.loads(body)
+    print("Callback")
+    print(f"Received message: {raw_type_message['type']}")
+
+    if str(raw_type_message['type']) == 'reviews':
+        try:
+            message = ReviewsMessage(**json.loads(body))
+            result = generate_by_reviews(message.reviews)
+            send_answer_to_reviews(True, "All reviews processed", task_pb2.CheckReviewsData(
+                id=message.id,
+                value=result
+                ))
+        except Exception as ex:
+            send_answer_to_reviews(False, f"Error generating reviews message: {ex}", task_pb2.CheckReviewsData(
+                id=message.id,
+                value=""
+                ))
+        finally:
+            return
+        
+    elif str(raw_type_message['type']) == 'seo_v1' and str(raw_type_message['type']) == 'seo_v2':
+        try:
+            message = SeoMessage(**json.loads(body))
+            result = ""
+            if message.type == 'seo_v1':
+                result = generate_new_text_with_seo_words_v1(message.product, message.competitors)
+            else:
+                result = generate_new_text_with_seo_words_v2(message.product, message.competitors)
+
+            send_answer_to_description(True, "Description processed",task_pb2.CheckDescriptionData(
+                id=message.id,
+                value=result
+                ))
+        except Exception as ex:
+            send_answer_to_description(False, f"Error generating description message: {ex}", task_pb2.CheckDescriptionData(
+                id=message.id,
+                value=""
+                ))
+        finally:
+            return   
+    
+
+
+def start_seo_consumer():
+    credentials = pika.PlainCredentials(os.getenv('RABBITMQ_LOGIN'), os.getenv('RABBITMQ_PASSWORD'))
+    connection = pika.BlockingConnection(pika.ConnectionParameters(os.getenv('RABBITMQ_HOST'), os.getenv('RABBITMQ_PORT'), credentials=credentials))
+    queue_name = 'seo_queue'
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange=queue_name, exchange_type='direct', durable=False)
+    channel.queue_declare(queue=queue_name, passive=True)
+
+    channel.basic_consume(
+        queue=queue_name,
+        on_message_callback=callback,
+        auto_ack=False 
+    )
+    print("Definition seo consumer waiting for messages...")
+    channel.start_consuming()
+
+if __name__ == "__main__":
+    start_seo_consumer()
