5676898de70acab2038f53ff27ed09b78bf78784
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-12-18 02:17:32 +0300

    xsdas

---
+++ start_all.sh
@@ -0,0 +1,4 @@
+sudo nohup python3 ai_check_photo/main.py > ai_check_photo.log 2>&1 &
+sudo nohup python3 ai_multy_modal/main.py > ai_multy_modal.log 2>&1 &
+sudo nohup python3 ai_text_operation/main.py > ai_text_operation.log 2>&1 &
+echo "Все скрипты запущены с правами sudo!"
