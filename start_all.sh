4e495bdbc1582f52671adc460ac851c6f3ee2e84
GoodGame563 <124364912+GoodGame563@users.noreply.github.com>
2024-12-23 16:47:11 +0300

    Update start_all.sh

--- start_all.sh
+++ start_all.sh
@@ -1,4 +1,4 @@
-sudo python3 -m pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+sudo python3 -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 sudo nohup python3 ai_check_photo/main.py > ai_check_photo.log 2>&1 &
 sudo nohup python3 ai_multy_modal/main.py > ai_multy_modal.log 2>&1 &
 sudo nohup python3 ai_text_operation/main.py > ai_text_operation.log 2>&1 &
